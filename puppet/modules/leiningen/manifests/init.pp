b70028d10b2e2c562662c2608b62449d74b90876
hokkaido <info@kimchi.io>
2014-04-14 23:42:30 +0200

    Fix Leiningen provisioning

--- puppet/modules/leiningen/manifests/init.pp
+++ puppet/modules/leiningen/manifests/init.pp
@@ -22,7 +22,7 @@ class leiningen (
     user    => $user,
     group   => $user,
     path    => ["/bin", "/usr/bin", "/usr/local/bin"],
-    cwd     => "/home/{$user}/bin",
+    cwd     => "/home/${user}/bin",
     command => "wget ${url} && chmod 755 lein",
     creates => "/home/$user/bin/lein",
     require => [File["create local-bin-folder"],
@@ -30,9 +30,8 @@ class leiningen (
   }
 
   exec {'exec_coolness':
-    command => 'lein',
-    cwd     => "/home/{$user}/bin",
-    creates => "/home/$user/.lein",
+    command => "/home/${user}/bin/lein",
+    creates => "/home/${user}/.lein",
     require => Exec["download leiningen"]
   }
 
