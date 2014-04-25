8ef17b96f804863173250b23c9dacc164bcbe250
hokkaido <info@kimchi.io>
2014-04-25 09:57:37 +0200

    Adding some deps

--- project.clj
+++ project.clj
@@ -9,7 +9,11 @@
                  [ring/ring-jetty-adapter "1.2.2"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-anti-forgery "0.3.1"]
-                 [compojure "1.1.6"]]
+                 [prismatic/plumbing "0.2.2"]
+                 [prismatic/schema "0.2.1"]
+                 [prismatic/fnhouse "0.1.0"]
+                 [compojure "1.1.6"]
+                 [crypto-password "0.1.3"]]
   :dev-dependencies [[ring/ring-devel "1.2.2"] [lein-light-nrepl "0.0.17"]]
   :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
   :main ^:skip-aot juno.server.cli
